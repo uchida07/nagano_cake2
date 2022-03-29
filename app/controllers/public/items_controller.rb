6979c8a7952240d32c2eb11fa440918f629f844f
EC2 Default User <ec2-user@ip-172-31-4-10.ec2.internal>
2022-03-29 11:53:24 +0000

    Initial Commit

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,5 +1,9 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.all
+    @items = Item.page(params[:page])
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 end
