cd4982aa5ddcdfc4d6e976e9a6887c9dff169be1
uchida07 <shunsukeu.ri1994@gmail.com>
2022-04-04 12:16:49 +0000

    Initial Commit

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,9 +1,19 @@
 class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
-    
+
+  end
+  def confirm
+    @orders = Order.all
   end
   def complete
 
   end
+  def index
+    @orders = Order.all
+  end
+  def show
+    @orders = Order.find(params[:id])
+  end
+
 end
