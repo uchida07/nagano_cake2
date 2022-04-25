d1fdde843fc38a7b58bee34070ff3265e6321ef4
uchida07 <shunsukeu.ri1994@gmail.com>
2022-04-25 11:25:56 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :customers, skip: [:passwords], controllers: {
+  devise_for :customer, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
